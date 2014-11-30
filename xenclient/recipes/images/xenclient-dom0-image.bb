OpenXT/xenclient-oe

commit d08f15bf88e5378b19bed88ce3e9bcc266c595a7

[OXT-23] Use common license directory for OXT recipes.
OXT has historically referenced the license in the TOPDIR of the
xenclient-oe repo. As we make this repo a meta layer TOPDIR ends up
pointing elsewhere. As such it's much easier to use the GPLv2 and MIT
license files from the common license dir provided by OE core.

xenclient/recipes/images/xenclient-dom0-image.bb
@@ -108,5 +108,5 @@ inherit xenclient-licences
 require xenclient-version.inc
 
 LICENSE = "GPLv2 & MIT"
-LIC_FILES_CHKSUM = "file://${TOPDIR}/COPYING.GPLv2;md5=751419260aa954499f7abaabaa882bbe      \
-                    file://${TOPDIR}/COPYING.MIT;md5=3da9cfbcb788c80a0384361b4de20420"
+LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/GPL-2.0;md5=751419260aa954499f7abaabaa882bbe      \
+                    file://${COMMON_LICENSE_DIR}/MIT;md5=3da9cfbcb788c80a0384361b4de20420"

xenclient/recipes/images/xenclient-initramfs-image.bb
@@ -73,5 +73,5 @@ inherit xenclient-image-src-info
 inherit xenclient-image-src-package
 
 LICENSE = "GPLv2 & MIT"
-LIC_FILES_CHKSUM = "file://${TOPDIR}/COPYING.GPLv2;md5=751419260aa954499f7abaabaa882bbe      \
-                    file://${TOPDIR}/COPYING.MIT;md5=3da9cfbcb788c80a0384361b4de20420"
+LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/GPL-2.0;md5=751419260aa954499f7abaabaa882bbe      \
+                    file://${COMMON_LICENSE_DIR}/MIT;md5=3da9cfbcb788c80a0384361b4de20420"

xenclient/recipes/images/xenclient-installer-image.bb
@@ -128,8 +128,8 @@ inherit xenclient-licences
 require xenclient-version.inc
 
 LICENSE = "GPLv2 & MIT"
-LIC_FILES_CHKSUM = "file://${TOPDIR}/COPYING.GPLv2;md5=751419260aa954499f7abaabaa882bbe      \
-                    file://${TOPDIR}/COPYING.MIT;md5=3da9cfbcb788c80a0384361b4de20420"
+LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/GPL-2.0;md5=751419260aa954499f7abaabaa882bbe      \
+                    file://${COMMON_LICENSE_DIR}/MIT;md5=3da9cfbcb788c80a0384361b4de20420"
 
 python() {
     bb.data.delVarFlag("do_fetch", "noexec", d);

xenclient/recipes/images/xenclient-installer-part2-image.bb
@@ -29,8 +29,8 @@ ROOTFS_POSTPROCESS_COMMAND += " \
     rm -rf ${IMAGE_ROOTFS}/usr;"
 
 LICENSE = "GPLv2 & MIT"
-LIC_FILES_CHKSUM = "file://${TOPDIR}/COPYING.GPLv2;md5=751419260aa954499f7abaabaa882bbe      \
-                    file://${TOPDIR}/COPYING.MIT;md5=3da9cfbcb788c80a0384361b4de20420"
+LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/GPL-2.0;md5=751419260aa954499f7abaabaa882bbe      \
+                    file://${COMMON_LICENSE_DIR}/MIT;md5=3da9cfbcb788c80a0384361b4de20420"
 
 # prevent ldconfig from being run
 LDCONFIGDEPEND = ""

xenclient/recipes/images/xenclient-ndvm-image.bb
@@ -86,5 +86,5 @@ inherit xenclient-licences
 require xenclient-version.inc
 
 LICENSE = "GPLv2 & MIT"
-LIC_FILES_CHKSUM = "file://${TOPDIR}/COPYING.GPLv2;md5=751419260aa954499f7abaabaa882bbe      \
-                    file://${TOPDIR}/COPYING.MIT;md5=3da9cfbcb788c80a0384361b4de20420"
+LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/GPL-2.0;md5=751419260aa954499f7abaabaa882bbe      \
+                    file://${COMMON_LICENSE_DIR}/MIT;md5=3da9cfbcb788c80a0384361b4de20420"

xenclient/recipes/images/xenclient-source-image.bb
@@ -32,8 +32,8 @@ ROOTFS_POSTPROCESS_COMMAND =+ " \
 "
 
 LICENSE = "GPLv2 & MIT"
-LIC_FILES_CHKSUM = "file://${TOPDIR}/COPYING.GPLv2;md5=751419260aa954499f7abaabaa882bbe      \
-                    file://${TOPDIR}/COPYING.MIT;md5=3da9cfbcb788c80a0384361b4de20420"
+LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/GPL-2.0;md5=751419260aa954499f7abaabaa882bbe      \
+                    file://${COMMON_LICENSE_DIR}/MIT;md5=3da9cfbcb788c80a0384361b4de20420"
 
 # prevent ldconfig from being run
 LDCONFIGDEPEND = ""

xenclient/recipes/images/xenclient-stubdomain-initramfs-image.bb
@@ -33,5 +33,5 @@ inherit xenclient-image-src-info
 inherit xenclient-image-src-package
 
 LICENSE = "GPLv2 & MIT"
-LIC_FILES_CHKSUM = "file://${TOPDIR}/COPYING.GPLv2;md5=751419260aa954499f7abaabaa882bbe      \
-                    file://${TOPDIR}/COPYING.MIT;md5=3da9cfbcb788c80a0384361b4de20420"
+LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/GPL-2.0;md5=751419260aa954499f7abaabaa882bbe      \
+                    file://${COMMON_LICENSE_DIR}/MIT;md5=3da9cfbcb788c80a0384361b4de20420"

xenclient/recipes/images/xenclient-syncvm-image.bb
@@ -52,5 +52,5 @@ inherit xenclient-licences
 require xenclient-version.inc
 
 LICENSE = "GPLv2 & MIT"
-LIC_FILES_CHKSUM = "file://${TOPDIR}/COPYING.GPLv2;md5=751419260aa954499f7abaabaa882bbe      \
-                    file://${TOPDIR}/COPYING.MIT;md5=3da9cfbcb788c80a0384361b4de20420"
+LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/GPL-2.0;md5=751419260aa954499f7abaabaa882bbe      \
+                    file://${COMMON_LICENSE_DIR}/MIT;md5=3da9cfbcb788c80a0384361b4de20420"

commit 37712b7d50c1cdfcefe9101bf1da1c7df7d3d425

[OXT-23] Fix license checksum for MIT licenses.
This was caused by d08f15bf88e5378b19bed88ce3e9bcc266c595a7 but went
unnoticed due to OXT-74.

xenclient/recipes/images/xenclient-dom0-image.bb
@@ -109,4 +109,4 @@ require xenclient-version.inc
 
 LICENSE = "GPLv2 & MIT"
 LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/GPL-2.0;md5=751419260aa954499f7abaabaa882bbe      \
-                    file://${COMMON_LICENSE_DIR}/MIT;md5=3da9cfbcb788c80a0384361b4de20420"
+                    file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"

xenclient/recipes/images/xenclient-initramfs-image.bb
@@ -74,4 +74,4 @@ inherit xenclient-image-src-package
 
 LICENSE = "GPLv2 & MIT"
 LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/GPL-2.0;md5=751419260aa954499f7abaabaa882bbe      \
-                    file://${COMMON_LICENSE_DIR}/MIT;md5=3da9cfbcb788c80a0384361b4de20420"
+                    file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"

xenclient/recipes/images/xenclient-installer-image.bb
@@ -133,7 +133,7 @@ require xenclient-version.inc
 
 LICENSE = "GPLv2 & MIT"
 LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/GPL-2.0;md5=751419260aa954499f7abaabaa882bbe      \
-                    file://${COMMON_LICENSE_DIR}/MIT;md5=3da9cfbcb788c80a0384361b4de20420"
+                    file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"
 
 python() {
     bb.data.delVarFlag("do_fetch", "noexec", d);

xenclient/recipes/images/xenclient-installer-part2-image.bb
@@ -30,7 +30,7 @@ ROOTFS_POSTPROCESS_COMMAND += " \
 
 LICENSE = "GPLv2 & MIT"
 LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/GPL-2.0;md5=751419260aa954499f7abaabaa882bbe      \
-                    file://${COMMON_LICENSE_DIR}/MIT;md5=3da9cfbcb788c80a0384361b4de20420"
+                    file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"
 
 # prevent ldconfig from being run
 LDCONFIGDEPEND = ""

xenclient/recipes/images/xenclient-ndvm-image.bb
@@ -87,4 +87,4 @@ require xenclient-version.inc
 
 LICENSE = "GPLv2 & MIT"
 LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/GPL-2.0;md5=751419260aa954499f7abaabaa882bbe      \
-                    file://${COMMON_LICENSE_DIR}/MIT;md5=3da9cfbcb788c80a0384361b4de20420"
+                    file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"

xenclient/recipes/images/xenclient-source-image.bb
@@ -33,7 +33,7 @@ ROOTFS_POSTPROCESS_COMMAND =+ " \
 
 LICENSE = "GPLv2 & MIT"
 LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/GPL-2.0;md5=751419260aa954499f7abaabaa882bbe      \
-                    file://${COMMON_LICENSE_DIR}/MIT;md5=3da9cfbcb788c80a0384361b4de20420"
+                    file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"
 
 # prevent ldconfig from being run
 LDCONFIGDEPEND = ""

xenclient/recipes/images/xenclient-stubdomain-initramfs-image.bb
@@ -34,4 +34,4 @@ inherit xenclient-image-src-package
 
 LICENSE = "GPLv2 & MIT"
 LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/GPL-2.0;md5=751419260aa954499f7abaabaa882bbe      \
-                    file://${COMMON_LICENSE_DIR}/MIT;md5=3da9cfbcb788c80a0384361b4de20420"
+                    file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"

xenclient/recipes/images/xenclient-syncvm-image.bb
@@ -53,4 +53,4 @@ require xenclient-version.inc
 
 LICENSE = "GPLv2 & MIT"
 LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/GPL-2.0;md5=751419260aa954499f7abaabaa882bbe      \
-                    file://${COMMON_LICENSE_DIR}/MIT;md5=3da9cfbcb788c80a0384361b4de20420"
+                    file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"

xenclient/recipes/images/xenclient-sysroot-image.bb
@@ -74,4 +74,4 @@ require xenclient-version.inc
 
 LICENSE = "GPLv2 & MIT"
 LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/GPL-2.0;md5=751419260aa954499f7abaabaa882bbe      \
-                    file://${COMMON_LICENSE_DIR}/MIT;md5=3da9cfbcb788c80a0384361b4de20420"
+                    file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"

xenclient/recipes/images/xenclient-uivm-image.bb
@@ -153,4 +153,4 @@ require xenclient-version.inc
 
 LICENSE = "GPLv2 & MIT"
 LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/GPL-2.0;md5=751419260aa954499f7abaabaa882bbe      \
-                    file://${COMMON_LICENSE_DIR}/MIT;md5=3da9cfbcb788c80a0384361b4de20420"
+                    file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"

xenclient/recipes/initrdscripts/initramfs-xenclient_1.0.bb
@@ -1,5 +1,5 @@
 LICENSE = "MIT"
-LIC_FILES_CHKSUM="file://${COMMON_LICENSE_DIR}/MIT;md5=3da9cfbcb788c80a0384361b4de20420"
+LIC_FILES_CHKSUM="file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"
 SRC_URI = "file://init.sh"
 PR = "r2"
 DESCRIPTON = "XenClient initramfs init script"

xenclient/recipes/modules/modules-installer_1.0.bb
@@ -1,7 +1,7 @@
 SECTION = "base"
 DESCRIPTION = "Package providing /etc/modules file"
 LICENSE = "MIT"
-LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=3da9cfbcb788c80a0384361b4de20420"
+LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"
 PACKAGE_ARCH = "${MACHINE_ARCH}"
 PR = "r1"
 

xenclient/recipes/modules/modules_1.0.bb
@@ -1,7 +1,7 @@
 SECTION = "base"
 DESCRIPTION = "Package providing /etc/modules file"
 LICENSE = "MIT"
-LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=3da9cfbcb788c80a0384361b4de20420"
+LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"
 PACKAGE_ARCH = "${MACHINE_ARCH}"
 PR = "r1"
 

xenclient/recipes/networkmanager/networkmanager-certs.bb
@@ -1,6 +1,6 @@
 DESCRIPTION = "Tool to copy NM certificates"
 LICENSE = "MIT"
-LIC_FILES_CHKSUM="file://${COMMON_LICENSE_DIR}/MIT;md5=3da9cfbcb788c80a0384361b4de20420"
+LIC_FILES_CHKSUM="file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"
 PACKAGE_ARCH = "${MACHINE_ARCH}"
 RDEPENDS_${PN} += "libtirpc libicbinn libicbinn-client xen-tools-xenstore-utils"
 

xenclient/recipes/ocaml/ocaml-findlib-config_0.1.bb
@@ -1,5 +1,5 @@
 LICENSE = "MIT"
-LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=3da9cfbcb788c80a0384361b4de20420"
+LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"
 PACKAGE_ARCH = "${MACHINE_ARCH}"
 PR = "r0"
 

xenclient/recipes/tasks/task-xenclient-common.bb
@@ -1,7 +1,7 @@
 DESCRIPTION = "Common packages for XenClient images"
 LICENSE = "GPLv2 & MIT"
 LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/GPL-2.0;md5=751419260aa954499f7abaabaa882bbe      \
-                    file://${COMMON_LICENSE_DIR}/MIT;md5=3da9cfbcb788c80a0384361b4de20420"
+                    file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"
 
 inherit xenclient
 inherit task

xenclient/recipes/tasks/task-xenclient-dom0.bb
@@ -1,7 +1,7 @@
 DESCRIPTION = "All packages required for XenClient dom0"
 LICENSE = "GPLv2 & MIT"
 LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/GPL-2.0;md5=751419260aa954499f7abaabaa882bbe      \
-                    file://${COMMON_LICENSE_DIR}/MIT;md5=3da9cfbcb788c80a0384361b4de20420"
+                    file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"
 
 inherit xenclient
 inherit task

xenclient/recipes/tasks/task-xenclient-extra.bb
@@ -1,7 +1,7 @@
 DESCRIPTION = "Packages build during extra step"
 LICENSE = "GPLv2 & MIT"
 LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/GPL-2.0;md5=751419260aa954499f7abaabaa882bbe      \
-                    file://${COMMON_LICENSE_DIR}/MIT;md5=3da9cfbcb788c80a0384361b4de20420"
+                    file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"
 
 inherit task
 

xenclient/recipes/tasks/task-xenclient-installer.bb
@@ -1,7 +1,7 @@
 DESCRIPTION = "All packages required for XenClient installer"
 LICENSE = "GPLv2 & MIT"
 LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/GPL-2.0;md5=751419260aa954499f7abaabaa882bbe      \
-                    file://${COMMON_LICENSE_DIR}/MIT;md5=3da9cfbcb788c80a0384361b4de20420"
+                    file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"
 PR = "r11"
 
 inherit task

xenclient/recipes/xenclient/xenclient-console-keymaps_1.0.bb
@@ -1,6 +1,6 @@
 DESCRIPTION = "Console keymaps for XenClient"
 LICENSE = "MIT"
-LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=3da9cfbcb788c80a0384361b4de20420"
+LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"
 DEPENDS = "console-setup-ckbcomp-native \
            xkeyboard-config \
            xenclient-keyboard-list"

xenclient/recipes/xenclient/xenclient-keyboard-list_1.0.bb
@@ -1,6 +1,6 @@
 DESCRIPTION = "XenClient keyboard list"
 LICENSE = "MIT"
-LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=3da9cfbcb788c80a0384361b4de20420"
+LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"
 DEPENDS = "xkeyboard-config"
 
 SRC_URI = "file://supported-keyboards \

xenclient/recipes/xenclient/xenclient-keymap-sync_1.0.bb
@@ -1,6 +1,6 @@
 DESCRIPTION = "XenClient keymap sync daemon"
 LICENSE = "MIT"
-LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=3da9cfbcb788c80a0384361b4de20420"
+LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"
 SRC_URI = "file://keymap-sync"
 RDEPENDS = "xenclient-keyboard-list"
 

xenclient/recipes/xenclient/xenclient-language-sync_1.0.bb
@@ -1,6 +1,6 @@
 DESCRIPTION = "XenClient language sync daemon"
 LICENSE = "MIT"
-LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=3da9cfbcb788c80a0384361b4de20420"
+LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"
 RDEPENDS_${PN} += "dbus"
 
 SRC_URI = "file://language-sync \

xenclient/recipes/xenclient/xenclient-pcrdiff_1.0.bb
@@ -1,6 +1,6 @@
 DESCRIPTION = "XenClient pcr diff tool"
 LICENSE = "MIT"
-LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=3da9cfbcb788c80a0384361b4de20420"
+LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"
 
 inherit xenclient
 

xenclient/recipes/xenclient/xenclient-uivm-xsessionconfig_0.0.2.bb
@@ -37,7 +37,7 @@ SRC_URI = "file://xinitrc \
 
 RDEPENDS += "xdotool"
 LICENSE = "MIT"
-LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=3da9cfbcb788c80a0384361b4de20420"
+LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"
 
 PR = "r26"
 